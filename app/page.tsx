a1728266252c02de6b1fd5a64ff27aa6165e23c4
Emeka Nkwocha <iemeka>
2024-12-02 22:54:52 +0000

    initial com

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 
 export default function Page() {
 	return (
-		<main className="flex justify-around min-h-screen flex-col p-6 border-2 border-red-500">
+		<main className="flex justify-around min-h-screen flex-col p-6">
 			<div className="flex flex-row justify-around gap-6 rounded-lg bg-gray-50 px-6 py-10">
 				<Link
 					className="flex items-center gap-5 self-center rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
